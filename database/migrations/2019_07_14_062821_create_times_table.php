99b75adf4538091e74c16eae037d73c2278c71f2
dora252 <nikofudora184@gmail.com>
2019-07-14 02:10:24 +0000

    table

--- database/migrations/2019_07_14_062821_create_times_table.php
+++ database/migrations/2019_07_14_062821_create_times_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTimesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('times', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password',60);
-            $table->boolean('admin_flag')->default(false);
-            $table->rememberToken();
+            $table->string('user_id');
+            $table->string('date');
+            $table->string('start_time');
+            $table->string('end_time');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('times');
     }
 }
